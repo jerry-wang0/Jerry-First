d8f6d105d5b56043e2c2d06bbe114ffb52209f7c
yangjianjia <dhc-jianjia.yang@dhc.com.cn>
2017-04-19 16:43:14 +0900

    fix

---
+++ app/src/main/java/jerry/test/com/mvp/widget/search/SearchView.java
@@ -0,0 +1,29 @@
+package jerry.test.com.mvp.widget.search;
+
+import jerry.test.com.mvp.view.View;
+
+/**
+ * Created by jerry on 2017/04/19.
+ */
+
+public class SearchView implements View {
+    @Override
+    public void Show() {
+
+    }
+
+    @Override
+    public void Hide() {
+
+    }
+
+    @Override
+    public void Update() {
+
+    }
+
+    @Override
+    public void Destory() {
+
+    }
+}
