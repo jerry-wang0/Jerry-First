5ef14c747c848632d847859f097a2ff8d146298c
yangjianjia <dhc-jianjia.yang@dhc.com.cn>
2017-04-19 16:54:50 +0900

    fix

---
+++ app/src/main/java/jerry/test/com/mvp/widget/settings/SettingsPresenter.java
@@ -0,0 +1,20 @@
+package jerry.test.com.mvp.widget.settings;
+
+import jerry.test.com.mvp.presenter.AbstractPresenter;
+import jerry.test.com.mvp.view.View;
+
+/**
+ * Created by jerry on 2017/04/19.
+ */
+
+public class SettingsPresenter extends AbstractPresenter {
+
+    public SettingsPresenter(View view) {
+        super(view);
+    }
+
+    @Override
+    public void onCreate() {
+
+    }
+}
