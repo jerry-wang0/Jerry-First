26fe52bc9f070465cbf3e6a4f85eacc473a42a18
yangjianjia <dhc-jianjia.yang@dhc.com.cn>
2017-04-25 10:59:34 +0900

    adding

---
+++ app/src/main/java/jerry/test/com/mvp/widget/settings/SettingView.java
@@ -0,0 +1,30 @@
+package jerry.test.com.mvp.widget.settings;
+
+import jerry.test.com.mvp.view.View;
+
+/**
+ * Created by jerry on 2017/04/25.
+ */
+
+public class SettingView implements View {
+
+    @Override
+    public void Show() {
+
+    }
+
+    @Override
+    public void Hide() {
+
+    }
+
+    @Override
+    public void Update() {
+
+    }
+
+    @Override
+    public void Destory() {
+
+    }
+}
