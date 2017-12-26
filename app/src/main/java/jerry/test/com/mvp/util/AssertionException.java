28d35fd6b7d9eee1beca0cccad41ad598c1a55cb
jerry_wang <wgl_jacky@hotmail.com>
2017-12-26 19:58:11 +0800

    add

---
+++ app/src/main/java/jerry/test/com/mvp/util/AssertionException.java
@@ -0,0 +1,29 @@
+package jerry.test.com.mvp.util;
+
+/**
+ * 
+ * @author Jingqi Xu
+ */
+public class AssertionException extends RuntimeException {
+	//
+	private static final long serialVersionUID = 7066468784796847964L;
+
+	/**
+	 * 
+	 */
+	public AssertionException() {
+		this(null, null);
+	}
+	
+	public AssertionException(String msg) {
+		this(msg, null);
+	}
+	
+	public AssertionException(Throwable cause) {
+		this(null, cause);
+	}
+	
+	public AssertionException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
